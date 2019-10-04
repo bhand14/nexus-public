c6d76162fd30cefedea6a6c78c36679cb69cfa9e
bhand14 <52184283+bhand14@users.noreply.github.com>
2019-10-03 21:53:49 -0400

    Add placeholder appendUserRoles method
    I just wanted to mark the exact location of where this new method will go. I need to figure out how to implement this.

--- components/nexus-security/src/main/java/org/sonatype/nexus/security/internal/SecurityApiImpl.groovy
+++ components/nexus-security/src/main/java/org/sonatype/nexus/security/internal/SecurityApiImpl.groovy
@@ -100,4 +100,11 @@ class SecurityApiImpl
     user.roles = roleIds.collect{ new RoleIdentifier(DEFAULT_SOURCE, it)}
     securitySystem.updateUser(user)
   }
+    
+  @Override
+  User appendUserRoles(final String userId, final List<String> roleIds) {
+      User user = securitySystem.getUser(userId, DEFAULT_SOURCE)
+      user.roles = roleIds.collect{ new RoleIdentifier(DEFAULT_SOURCE, it)} // maybe it's this that needs to change?
+  
+  }
 }
